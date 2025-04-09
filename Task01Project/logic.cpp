db2f99bc97d7c40815732b36cdf3381448be2485
Матвей <matvey.myown@gmail.com>
2025-04-09 15:12:04 +0300

    Update logic

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -1,9 +1,11 @@
-int get_quantity_of_days (int month)
+int get_quantity_of_days(int month)
 {
 	if (month < 1 || month > 12)
 	{
 		return 0;
 	}
 
-	return month == 2 ? 28 : month % 2 == 0 ? 30 : 31;
+	int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	return days[month - 1];
 }
